ea7f78f9daa85c23734f03db9fbd9bb0281bfae4
Araks Miskarova <araksmiskarova@gmail.com>
2023-07-08 13:00:58 +0200

    Password eye is added

--- client/src/pages/LogInPage/LogInPage.jsx
+++ client/src/pages/LogInPage/LogInPage.jsx
@@ -5,6 +5,10 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import { useForm } from 'react-hook-form';
 import { useNavigate, NavLink } from 'react-router-dom';
+import InputAdornment from '@mui/material/InputAdornment';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+// import IconButton from '@mui/material/IconButton';
 
 import { useDispatch } from 'react-redux';
 import { fetchUserToken } from '../../redux/slices/authorization';
@@ -22,6 +26,7 @@ function LogInPage() {
 
   const [passError, setPassError] = useState([]);
   const [status, setStatus] = useState(false);
+  const [togglePassword, setTogglePassword] = useState(false);
 
   const onSubmit = async values => {
     setStatus(true);
@@ -91,15 +96,19 @@ function LogInPage() {
     }
   };
 
+  const toggleData = () => {
+    setTogglePassword(!togglePassword);
+  };
+
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm({
-    defaultValues: {
-      loginOrEmail: 'test@test.ua',
-      password: '1234567',
-    },
+    // defaultValues: {
+    //   loginOrEmail: 'test@test.ua',
+    //   password: '1234567',
+    // },
     mode: 'onChange',
   });
 
@@ -126,9 +135,28 @@ function LogInPage() {
             className={style.field}
             label="Password"
             fullWidth
+            type={!togglePassword ? 'password' : 'text'}
             error={Boolean(errors.password?.message)}
             helperText={errors.password?.message}
             {...register('password', { required: 'Enter your password' })}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {' '}
+                  {togglePassword ? (
+                    <VisibilityIcon
+                      className={style.cursor_pointer}
+                      onClick={toggleData}
+                    />
+                  ) : (
+                    <VisibilityOffIcon
+                      className={style.cursor_pointer}
+                      onClick={toggleData}
+                    />
+                  )}
+                </InputAdornment>
+              ),
+            }}
           />
           <div
             className={
