258a6d3edfa828b3670ededeedf5689892798246
Araks Miskarova <araksmiskarova@gmail.com>
2023-07-11 19:51:41 +0200

    deleted client

--- client/src/helpers/authentication/authentication.js
+++
@@ -1,9 +0,0 @@
-export const isToken = () => {
-  const token = localStorage.getItem('token');
-  return token !== null;
-};
-
-export const isAuthenticated = () => {
-  const customer = localStorage.getItem('customer');
-  return customer !== null;
-};
